ff6592404eed6e4ac834c0d24c04da883fe05425
Albert Lee <allee@guardanthealth.com>
2021-12-22 14:43:09 -0800

    make it work with creds

--- poetry/factory.py
+++ poetry/factory.py
@@ -252,6 +252,13 @@ class Factory:
         else:
             auth = None
 
+        _http_basic = auth_config.get("http-basic")
+        if _http_basic is not None:
+            _username = _http_basic.get(name).get("username")
+            _password = _http_basic.get(name).get("password")
+            protocol, uri = url.split("//", 1)
+            url = f"{protocol}//{_username}:{_password}@{uri}"
+
         return LegacyRepository(
             name,
             url,
